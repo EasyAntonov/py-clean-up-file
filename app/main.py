20f5b6416319c59df587efd06c0f6115eb68500b
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-14 23:42:30 +0300

    first try

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
         os.remove(self.filename)
